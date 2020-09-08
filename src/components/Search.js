1d76977951fd566acca10ecedbecd0884d0bd9f9
BlueC <bluecaban@gmail.com>
2020-09-07 21:27:44 -0700

    init

---
+++ src/components/Search.js
@@ -0,0 +1,83 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const Search = () => {
+    const [term, setTerm] = useState('comet');
+    const [debouncedTerm, setDebouncedTerm] = useState(term);
+    const [results, setResults] = useState([]);
+
+    // runs when term changes from input
+    // que up change in 1 sec if to quick reset timer
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+            setDebouncedTerm(term);
+        }, 1000);
+
+        return () => {
+            clearTimeout(timerId);
+        };
+    }, [term]);
+
+
+    //search update piece of state
+    //component runs when first rendered
+    useEffect(() => {
+        const search = async () => {
+            const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+                params: {
+                    action: 'query',
+                    list: 'search',
+                    origin: '*',
+                    format: 'json',
+                    srsearch: debouncedTerm,
+                },
+            });
+
+            setResults(data.query.search);
+        };
+        search();
+    }, [debouncedTerm]);
+
+    const renderedResults = results.map((results) => {
+        return ( 
+            <div key={results.pageid} className="item">
+                <div className="right floated content">
+                    <a 
+                        className="ui button"
+                        href={`https://en.wikipedia.org?curid=id=${results.pageid}`}    
+                    >
+                        Go
+                    </a>
+                </div>
+                <div className="content">
+                    <div className="header">
+                        {results.title}
+                    </div>
+                    <span dangerouslySetInnerHTML={{ __html: results.snippet }}></span>
+                </div>
+            </div>
+        );
+    });
+
+    return (
+        <div>
+            <div className="ui form">
+                <div className="field">
+                    <label>
+                        Enter Search Term
+                    </label>
+                    <input
+                        value={term}
+                        onChange={e => setTerm(e.target.value)} 
+                    className="input" 
+                    />
+                </div>
+            </div>
+            <div className="ui celled list">
+                {renderedResults}
+            </div>
+        </div>
+    );
+};
+
+export default Search;
